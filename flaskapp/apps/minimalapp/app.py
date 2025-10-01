15a13aaa1c40498dee803c277371c024acee31de
CW <79497457+antladybug@users.noreply.github.com>
2025-10-02 07:38:17 +0900

    Update app.py

--- flaskapp/apps/minimalapp/app.py
+++ flaskapp/apps/minimalapp/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask( __name__)
 
-@app.route( "/" )
-def index():
-    return "Hello, flaskbook!"
+@app.route( "/hello/<name>",
+            methods = ["GET", "POST"],
+            endpoint = "hello-endpoint" )
+def hello(name):
+    return f"Hello, { name }!"
