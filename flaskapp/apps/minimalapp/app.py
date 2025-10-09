27952bf474871cfebf73dae5bfc2ebdf258c6ed1
CW <79497457+antladybug@users.noreply.github.com>
2025-10-09 15:54:51 +0900

    Update app.py

--- flaskapp/apps/minimalapp/app.py
+++ flaskapp/apps/minimalapp/app.py
@@ -1,47 +1,47 @@
-from flask import Flask, current_app, g, render_template, url_for, request, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from email_validator import validate_email, EmailNotValidError
+from flask import Flask, render_template, request, url_for, redirect, flash
+import logging
 
-app = Flask( __name__)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
-@app.route( "/" )
-def index():
-	return "Hello, Flaskbook!";
-
-@app.route( "/hello/<name>",
-            methods = ["GET", "POST"],
-            endpoint = "hello-endpoint" )
-def hello(name):
-    return f"Hello, { name }!"
-
-@app.route( "/name/<name>" )
-def show_name( name ):
-    # 変数をテンプレートエンジンに渡す
-    return render_template( "index.html", name = name )
-
-with app.test_request_context():
-    print ( url_for( "index" ) )
-    print ( url_for( "hello-endpoint", name = "world" ) )
-    print ( url_for( "show_name", name = "ichiro", page = "1" ) )
-    
-# ctx = app.app_context()
-# ctx.push()
-
-# print ( current_app.name )
-
-# g.connection = "connection"
-# print ( g.connection )
-with app.test_request_context ( "/users?update = true" ):
-    # trueが出力される
-    print ( request.args.get( "updated" ) )
+app.logger.setLevel( logging.DEBUG )
+app.logger.critical( "fatal error" )
+app.logger.warning( "warning" )
+app.logger.info( "info" )
+app.logger.debug( "debug" )
+app.config[ "DRBUG_TB_INTERCEPT_REDIRECTS" ] = False
+toolbar = DebugToolbarExtension( app )
 
 @app.route( "/contact" )
 def contact():
-    return render_template( "contact.html" )
-
-@app.route( "/contact/complete", methods = [ "GET", "POST" ] )
-def contact_complete(): 
-    if request.method == "POST":
-        # メールを送る
-        
-        # contactエンドポイントへリダイレクトする
-        return redirect( url_for ( "contact_complete" ) )
-    return render_template( "contact_complete.html" )
+	return render_template ( "contact.html" )
+	
+@app.route( "/contact/complete", methods = [ "GET", "POST" ] )	
+def contact_complete():
+	if request.method == "POST":
+		username = request.form[ "username" ]
+		email = request.form [ "email" ]
+		description = request.form [ "description" ]
+		is_valid = True
+		if not username:
+			flash ( "ユーザ名は必須です" )
+			is_valid = False
+		if not email:
+			flash ( "メールアドレスは必須です" )
+			is_valid = False
+		try:
+			validate_email( email )
+		except EmailNotValidError:
+			flash ( "メールアドレスの形式で入力してください" )
+			is_valid = False
+		if not description:
+			flash ( "問い合わせ内容は必須です" )
+			is_valid = False
+		if not is_valid:
+			return redirect ( url_for ( "contact" ) )
+		flash( "問い合わせ内容はメールにて送信しました。問い合わせありがとうございました。" )
+		return redirect ( url_for ( "contact_complete" ) )
+	return render_template ( "contact_complete.html" )
+	
